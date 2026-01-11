3741a961257195bf9abdad2cee13f449ce5365db
SLAYER-MD <danieliithothekingpromo.09@gmail.com>
2026-01-11 10:10:33 -0500

    Add template response plugin structure

---
+++ plugins/_templateResponse.js
@@ -0,0 +1,13 @@
+export default {
+  command: [],
+  tags: "",
+  desc: "",
+  group: false,
+  admin: false,
+  owner: false,
+
+  run: async (sock, m, { args, text, prefix, command, config }) => {
+    // lógica del plugin
+  }
+}
+
