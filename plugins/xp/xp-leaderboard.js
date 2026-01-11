16f00512c2be9073e2e89b32dc829e0a7c103de2
SLAYER-MD <danieliithothekingpromo.09@gmail.com>
2026-01-11 10:22:01 -0500

    Create xp-leaderboard.js

---
+++ plugins/xp/xp-leaderboard.js
@@ -0,0 +1,20 @@
+export default {
+  command: ["rank", "top"],
+  tags: "xp",
+
+  run: async (sock, m) => {
+    const users = Object.entries(global.db.users)
+      .sort((a, b) => b[1].xp - a[1].xp)
+      .slice(0, 10)
+
+    let text = "🏆 TOP XP\n\n"
+    users.forEach(([jid, u], i) => {
+      text += `${i + 1}. @${jid.split("@")[0]} → Lv.${u.level} (${u.xp} XP)\n`
+    })
+
+    await sock.sendMessage(m.chat, {
+      text,
+      mentions: users.map(u => u[0])
+    })
+  }
+}
