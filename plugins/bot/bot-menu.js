eece89904332e5693f6e5efa1d593307f7354be0
SLAYER-MD <danieliithothekingpromo.09@gmail.com>
2026-01-11 10:11:59 -0500

    Add bot menu command with help options

---
+++ plugins/bot/bot-menu.js
@@ -0,0 +1,19 @@
+export default {
+  command: ["menu", "help"],
+  tags: "bot",
+  desc: "Menú principal",
+
+  run: async (sock, m, { config }) => {
+    const menu = `
+🛰️ ${config.botName}
+━━━━━━━━━━━━
+• .menu
+• .ping
+• .sticker
+• .kick
+• .translate
+━━━━━━━━━━━━
+    `
+    await sock.sendMessage(m.chat, { text: menu })
+  }
+}
