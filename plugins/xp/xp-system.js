b1cb60fde45f93127842ad9367de2be7d1bf7752
SLAYER-MD <danieliithothekingpromo.09@gmail.com>
2026-01-11 10:21:14 -0500

    Create xp-system.js

---
+++ plugins/xp/xp-system.js
@@ -0,0 +1,23 @@
+import { addXP } from "../../lib/leveling.js"
+
+export default {
+  custom: true,
+
+  run: async (sock, m) => {
+    if (!m.chat.endsWith("@g.us")) return
+
+    const db = global.db
+    const user = db.users[m.sender] ||= { xp: 0, level: 0 }
+
+    const oldLevel = user.level
+    addXP(user, 5)
+
+    if (user.level > oldLevel) {
+      await sock.sendMessage(m.chat, {
+        text: `🎉 @${m.sender.split("@")[0]} subió a nivel ${user.level}`,
+        mentions: [m.sender]
+      })
+    }
+  }
+}
+
